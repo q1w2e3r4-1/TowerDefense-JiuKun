a47fe5a3ae62195d38defa907f122725eba10b98
q1w2e3r4 <709787749@qq.com>
2025-11-25 00:51:20 +0800

    add tag for lora

--- serve_lora.sh
+++ serve_lora.sh
@@ -3,9 +3,10 @@
 set -x
 MODEL_PATH="$1"
 LORA_PATH="$2"
+LORA_TAG=${3:-"default"}
 PORT=8000
 HOST=127.0.0.1
-EXTRA_ARGS="${@:3}"
+EXTRA_ARGS="${@:4}"
 
 if [ -z "$MODEL_PATH" ]; then
   echo "用法: $0 <model_path> [extra_args...]"
@@ -13,4 +14,4 @@ if [ -z "$MODEL_PATH" ]; then
 fi
 
 vllm serve "$MODEL_PATH" --enable-lora \
-    --lora-modules qwen-lora=$LORA_PATH --port "$PORT" --host "$HOST" --max-model-len 65536 $EXTRA_ARGS
+    --lora-modules qwen-lora-$LORA_TAG=$LORA_PATH --port "$PORT" --host "$HOST" --max-model-len 65536 $EXTRA_ARGS
