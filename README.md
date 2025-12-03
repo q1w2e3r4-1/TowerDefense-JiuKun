51a980534f44478c9fc08cd3d863f5a670bd5ade
q1w2e3r4 <709787749@qq.com>
2025-12-03 09:17:06 +0800

    got 285.33

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 bash serve_lora.sh /home/ws/Qwen3-4B /home/ws/lora-v2 4B-675
 python ./client_r.py
+
+bash serve_lora.sh /home/ws/Qwen3-4B /ckpt/lora-1203/checkpoint-564 4B-1203
--- update_data.sh
+++ update_data.sh
@@ -1,9 +1,14 @@
 set -x
-python finetune/data_preprocess.py --input_dir /home/ws/data/game/merge --output_dir /home/LLaMA-Factory/data --output_name kun.json
+# python finetune/data_preprocess.py --input_dir /home/ws/data/game/merge --output_dir /home/LLaMA-Factory/data --output_name kun.json
 
-# for debug
-python finetune/data_preprocess.py --input_dir /home/ws/data/game/merge --output_dir /home/ws/data/game/merge --output_name kun.json
+# # for debug
+# python finetune/data_preprocess.py --input_dir /home/ws/data/game/merge --output_dir /home/ws/data/game/merge --output_name kun.json
 
 # cot
 # python finetune/data_preprocess.py --input_dir /home/ws/cot --output_dir /home/ws/cot --output_name kun_cot.json --cot
 # python finetune/data_preprocess.py --input_dir /home/ws/cot --output_dir /home/LLaMA-Factory/data --output_name kun_cot.json --cot
+
+python finetune/data_preprocess.py --input_dir /home/ws/data/game/train --output_dir /home/LLaMA-Factory/data --output_name kun.json
+
+# for debug
+python finetune/data_preprocess.py --input_dir /home/ws/data/game/train --output_dir /home/ws/data/game/train --output_name kun.json
